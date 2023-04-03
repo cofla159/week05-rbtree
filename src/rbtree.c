3a8b3262bc1c89982506ebf64b6a5113ec2d9243
Ubuntu <ubuntu@ip-172-31-0-254.ap-northeast-2.compute.internal>
2023-04-03 07:03:59 +0000

    init 성공

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,44 +1,73 @@
 #include "rbtree.h"
-
 #include <stdlib.h>
+#include <stdio.h>
 
-rbtree *new_rbtree(void) {
+rbtree *new_rbtree(void)
+{
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
+  if (p == NULL)
+  {
+    return NULL;
+  }
+  p->root = (node_t *)calloc(1, sizeof(node_t));
+  if (p->root == NULL)
+  {
+    free(p);
+    return NULL;
+  }
+  p->root->color = RBTREE_BLACK;
+  node_t *sentinel_node = (node_t *)calloc(1, sizeof(node_t));
+  if (sentinel_node == NULL)
+  {
+    free(p);
+    free(p->root);
+    return NULL;
+  }
+  sentinel_node->color = RBTREE_BLACK;
+#define SENTINEL sentinel_node
+  p->nil = SENTINEL;
+  p->root = p->nil;
   return p;
 }
 
-void delete_rbtree(rbtree *t) {
+void delete_rbtree(rbtree *t)
+{
   // TODO: reclaim the tree nodes's memory
   free(t);
 }
 
-node_t *rbtree_insert(rbtree *t, const key_t key) {
+node_t *rbtree_insert(rbtree *t, const key_t key)
+{
   // TODO: implement insert
   return t->root;
 }
 
-node_t *rbtree_find(const rbtree *t, const key_t key) {
+node_t *rbtree_find(const rbtree *t, const key_t key)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_min(const rbtree *t) {
+node_t *rbtree_min(const rbtree *t)
+{
   // TODO: implement find
   return t->root;
 }
 
-node_t *rbtree_max(const rbtree *t) {
+node_t *rbtree_max(const rbtree *t)
+{
   // TODO: implement find
   return t->root;
 }
 
-int rbtree_erase(rbtree *t, node_t *p) {
+int rbtree_erase(rbtree *t, node_t *p)
+{
   // TODO: implement erase
   return 0;
 }
 
-int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
+int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
+{
   // TODO: implement to_array
   return 0;
 }
